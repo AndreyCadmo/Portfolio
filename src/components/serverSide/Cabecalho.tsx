a8609b46a0e5d9cb21d49088092c3fd30ed4356c
Andrey Cadmo <andreycadmo06@gmail.com>
2024-08-07 16:34:02 -0300

    css do loading e criando pré-definições

--- src/components/serverSide/Cabecalho.tsx
+++ src/components/serverSide/Cabecalho.tsx
@@ -1,11 +1,11 @@
-
 import { IconHomeFilled } from "@tabler/icons-react";
 import ButtonCabecalho from "../clientSide/ButtonCabecalho";
 
 export default function Cabecalho() {
+    
     return (
-        <div className="flex bg-[#0D0F17] p-2 justify-between items-center">
-            <ButtonCabecalho link="/"><img className="w-8" src="images/logos/Ac.png" alt="Andrey Cadmo" /></ButtonCabecalho>
+        <div className="flex bg-header p-2 justify-between items-center z-10 relative">
+            <ButtonCabecalho link="/"><img className="w-8" src="imagens/logos/Ac.png" alt="Andrey Cadmo" /></ButtonCabecalho>
             <div className="flex gap-8">
                 <ButtonCabecalho link="/home"><IconHomeFilled size={18}/></ButtonCabecalho>
                 <ButtonCabecalho link="/about">Sobre</ButtonCabecalho>
