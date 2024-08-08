e4b8a8febf0320b8532574f907714c3b0481e413
Andrey Cadmo <andreycadmo06@gmail.com>
2024-08-08 15:13:39 -0300

    Criando DarkMode e LightMode, com next-themes e typography tailwind

---
+++ src/components/clientSide/WaveContainer/WaveComponent.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { Wave2 } from "@/components/serverSide/svg/Wave";
+import { useTheme } from "next-themes";
+import dynamic from "next/dynamic";
+import { ReactNode, useMemo } from "react";
+import Loading from "../Loading";
+
+type WaveComponentProps = {
+    lightColor: string;
+    darkColor: string;
+    children?: ReactNode;
+    isInvert?: boolean;
+}
+
+export function WaveComponent({
+    lightColor,
+    darkColor,
+    children,
+    isInvert = false
+}: WaveComponentProps) {
+    const { theme, systemTheme } = useTheme();
+
+    const fillColor = useMemo(() => {
+        if(!theme && typeof window !== "undefined") {
+            return localStorage?.getItem("theme") === "dark" ? darkColor : lightColor
+        } else if (theme === 'system') {
+            return systemTheme === "dark" ? darkColor : lightColor
+        }
+
+        return theme === 'dark' ? darkColor : lightColor
+    }, [theme, systemTheme]);
+
+    if (isInvert)
+        return (
+            <div className="h-dvh relative">
+                <Wave2 className="left-0 top-0 scale-y-[-1] scale-x-[-1]" fill={fillColor} />
+                {children}
+                <Wave2 className="right-0 bottom-0" fill={fillColor} />
+            </div>)
+
+    return (
+        <section className="min-h-[93vh] flex max-sm:flex-col justify-between relative">
+            <Wave2 className="right-0 top-0 scale-y-[-1] " fill={fillColor} />
+            {children}
+            <Wave2 className="left-0 bottom-0 scale-x-[-1]" fill={fillColor} />
+        </section>
+    )
+}
+
+export const WaveComponentFullClient =  dynamic(() => Promise.resolve(WaveComponent), {
+    loading: () => <Loading />,
+    ssr: false
+});
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,23 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: ["class"],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))"
       },
+      backgroundColor: {
+        "backDark": "#121212;",
+        "backLight": "#636571;",
+        "header": "#0D0F17;",
+        "header2": "#6b7280;",
+        "svgDark": "#212435",
+        "svgLight": "#888E99",
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography')],
 };
 export default config;
