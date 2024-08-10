5764be4205a39c881444c71d6bd11f83e0b4d3cf
Andrey Cadmo <andreycadmo06@gmail.com>
2024-08-10 00:31:44 -0300

    criando a página projetos

--- src/app/(interno)/projects/page.tsx
+++ src/app/(interno)/projects/page.tsx
@@ -1,8 +1,15 @@
+import { WaveComponentFullClient } from "@/components/clientSide/WaveContainer/WaveComponent";
+import { ProjectsList } from "@/components/serverSide/projects/ProjectCard";
+
+
 export default function Page() {
   return (
     <div>
-      <h1>Projects</h1>
-      <p>This is the projects page</p>
+      <div className="min-h-72 border-2 bg-gradient-to-b from-slate-400 dark:from-slate-900 dark:border-white border-black rounded-b-lg  justify-center flex flex-col items-center gap-5">
+        <h1 className="text-6xl">Meus Projetos</h1>
+        <p>Vamos ver oque eu já fiz até agora?</p>
+      </div>
+      <ProjectsList />
     </div>
   );
 }
