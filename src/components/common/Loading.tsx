8febbf579d8aff18b58911ed9a279589986ed2c2
Andrey Cadmo <andreycadmo06@gmail.com>
2026-06-29 21:44:40 -0300

    Chore: Mudando arquivo de local

---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+    return (
+        <section className="dots-container">
+            <div className="dot"></div>
+            <div className="dot"></div>
+            <div className="dot"></div>
+        </section>
+
+    )
+}
