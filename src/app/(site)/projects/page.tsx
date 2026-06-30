73aaf182fb3f25ab66ad00faddbef4c6c9d28d45
Andrey Cadmo <andreycadmo06@gmail.com>
2026-06-29 22:02:34 -0300

    style: removing redundant background color class from Projects page

--- src/app/(site)/projects/page.tsx
+++ src/app/(site)/projects/page.tsx
@@ -2,7 +2,7 @@ import { ProjectsList } from "@/components/common/ProjectCard";
 
 export default function Page() {
   return (
-    <div className="min-h-screen bg-backLight dark:bg-backDark transition-colors duration-300 bg-zinc-950"  >
+    <div className="min-h-screen bg-backLight dark:bg-backDark transition-colors duration-300"  >
       <div className="min-h-72 border-b-2 bg-gradient-to-b from-slate-400 dark:from-slate-900 dark:border-white border-black rounded-b-lg justify-center flex flex-col items-center gap-5">
         <h1 className="md:text-6xl text-5xl font-bold">Meus Projetos</h1>
         <p className="text-gray-600 dark:text-gray-400">Vamos ver o que eu já fiz até agora?</p>
